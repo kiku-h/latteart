aac29907f39366e0b5134ba1ca4e2432776b3e4c
Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>
2023-03-30 13:00:29 +0900

    refactor test
    Signed-off-by: Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>

--- packages/latteart-repository/tests/unit/services/ProjectExportService.spec.ts
+++ packages/latteart-repository/tests/unit/services/ProjectExportService.spec.ts
@@ -41,7 +41,7 @@ describe("ProjectExportService", () => {
 
     const testResultData = {
       id: "testResultId",
-      name: "testResulttName",
+      name: "testResultName",
       startTimeStamp: 0,
       lastUpdateTimeStamp: 0,
       initialUrl: "",
@@ -49,6 +49,24 @@ describe("ProjectExportService", () => {
       testSteps: [],
       coverageSources: [],
     };
+
+    const dailyTestProgress = [
+      {
+        date: "2023/01/01",
+        storyProgresses: [
+          {
+            storyId: "storyId",
+            testMatrixId: "testMatrixId",
+            testTargetGroupId: "testTargetGroupId",
+            testTargetId: "testTargetId",
+            viewPointId: "viewPointId",
+            plannedSessionNumber: 1,
+            completedSessionNumber: 0,
+            incompletedSessionNumber: 1,
+          },
+        ],
+      },
+    ];
 
     const projectService: ProjectsService = {
       getProjectIdentifiers: jest.fn(),
@@ -73,7 +91,7 @@ describe("ProjectExportService", () => {
     };
 
     const exportFileRepositoryService: ExportFileRepositoryService = {
-      exportProject: jest.fn(),
+      exportProject: jest.fn().mockResolvedValue("project.zip"),
       exportTestResult: jest.fn(),
     };
 
@@ -81,11 +99,14 @@ describe("ProjectExportService", () => {
       registerStoryTestProgresses: jest.fn(),
       registerProjectTestProgresses: jest.fn(),
       saveTodayTestProgresses: jest.fn(),
-      collectStoryDailyTestProgresses: jest.fn(),
+      collectStoryDailyTestProgresses: jest
+        .fn()
+        .mockResolvedValue(dailyTestProgress),
       collectProjectDailyTestProgresses: jest.fn(),
     };
 
-    it("includeProject: true, includeTestResults: true", async () => {
+    it("includeProject: true, includeTestResults: trueの場合、ProjectとTestResultのexport処理が実行される", async () => {
+      const service = new ProjectExportService();
       projectService.getProject = jest.fn().mockResolvedValue(projectData);
       testResultService.collectAllTestStepScreenshots = jest
         .fn()
@@ -98,20 +119,21 @@ describe("ProjectExportService", () => {
 
       await getRepository(TestResultEntity).save(new TestResultEntity());
 
-      await new ProjectExportService().export("1", true, true, {
+      const result = await service.export("1", true, true, {
         projectService,
         testResultService,
         exportFileRepositoryService,
         testProgressService,
       });
 
+      expect(result).toEqual("project.zip");
       expect(projectService.getProject).toBeCalledTimes(1);
       expect(testResultService.collectAllTestStepScreenshots).toBeCalledTimes(
         1
       );
     });
 
-    it("includeProject: false, includeTestResults: false", async () => {
+    it("includeProject: false, includeTestResults: falseの場合、ProjectとTestResultのexport処理が共に実行しない", async () => {
       projectService.getProject = jest.fn();
       testResultService.collectAllTestStepScreenshots = jest.fn();
 
@@ -128,6 +150,90 @@ describe("ProjectExportService", () => {
       expect(testResultService.collectAllTestStepScreenshots).toBeCalledTimes(
         0
       );
+    });
+
+    it("extractProjectExportDataで、ProjectのexportDataを返す", async () => {
+      const service = new ProjectExportService();
+      projectService.getProject = jest.fn().mockResolvedValue(projectData);
+      testProgressService.collectStoryDailyTestProgresses = jest
+        .fn()
+        .mockResolvedValue(dailyTestProgress);
+      const project = await service["extractProjectExportData"](
+        projectData.id,
+        {
+          projectService,
+          testProgressService,
+        }
+      );
+
+      const a = {
+        ...projectData,
+        version: 1,
+      };
+
+      expect(project).toEqual({
+        projectId: "projectId",
+        projectFile: {
+          fileName: "project.json",
+          data: JSON.stringify(a),
+        },
+        stories: [
+          {
+            storyId: "storyId",
+            sessions: [
+              {
+                sessionId: "sessionId",
+                attachedFiles: [{ fileUrl: "fileUrl" }],
+              },
+            ],
+          },
+        ],
+        progressesFile: {
+          fileName: "progress.json",
+          data: JSON.stringify(dailyTestProgress),
+        },
+      });
+    });
+
+    it("extractTestResultsExportDataで、TestRsultのexportDataを返す", async () => {
+      const service = new ProjectExportService();
+      projectService.getProject = jest.fn().mockResolvedValue(projectData);
+      testResultService.collectAllTestStepScreenshots = jest
+        .fn()
+        .mockResolvedValue([
+          {
+            id: "id",
+            fileUrl: "fileUrl",
+          },
+        ]);
+
+      await getRepository(TestResultEntity).save(new TestResultEntity());
+
+      const testResult = await service["extractTestResultsExportData"]({
+        testResultService,
+      });
+
+      expect(testResult).toEqual([
+        {
+          testResultId: "testResultId",
+          testResultFile: {
+            fileName: "log.json",
+            data: JSON.stringify({
+              version: 2,
+              name: "testResultName",
+              sessionId: "testResultId",
+              startTimeStamp: 0,
+              lastUpdateTimeStamp: 0,
+              initialUrl: "",
+              testingTime: 0,
+              history: {},
+              notes: [],
+              coverageSources: [],
+            }),
+          },
+          screenshots: [{ id: "id", fileUrl: "fileUrl" }],
+        },
+      ]);
     });
   });
 });
